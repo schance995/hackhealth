e3faf8351c14b6406a246a86f30784db87403728
Spencer Chan <54919210+schance995@users.noreply.github.com>
2021-02-13 16:56:38 -0500

    request updated

--- yale.py
+++ yale.py
@@ -11,14 +11,20 @@ table_SWL = pd.read_html(root_SWL.find("table").prettify())
 df = table_SWL[0]
 categories = df.iloc[:,0][1:]
 cases = df.iloc[:,1][1:]
-API_KEY='raspi-rice-3107'
+API_KEY='raspy-rice-3107'
+ENTRY_ID='5338939a-5ae3-4750-a701-40bf14726909'
 for data, value in zip(categories, cases):
     # api push to set the key to value
     data = {
             'key': API_KEY,
             'data': data.lower(),
-            'value': value
+            'entry': ENTRY_ID,
+            'value': str(value)
             }
     print(data)
-    x = requests.post(url='https://console.echoAR.xyz/', data=data)
-    print(x)
+    response = requests.post(url='https://console.echoAR.xyz/post', data=data)
+    print(response)
+    print(response.request.url)
+    print(response.request.body)
+    print(response.request.headers)
+

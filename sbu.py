da530432c5c5b33cc1f7970bbc39d62d09ebbc50
Spencer Chan <54919210+schance995@users.noreply.github.com>
2021-02-13 14:03:18 -0500

    changed url and close browser when done

--- sbu.py
+++ sbu.py
@@ -15,9 +15,12 @@ from selenium import webdriver
 #soup = BeautifulSoup(html)
 
 driver = webdriver.Firefox()
-driver.get("https://tableau.stonybrook.edu/vizql/t/Public/w/Covid-19Accessible/v/WeeklyCases/viewData/sessions/1FCBAC071FD84953A29E128E587A10A2-1:2/views/14068672024067378307_6414820709505016832?maxrows=200&viz=%7B%22worksheet%22%3A%22SBU")
+driver.get('https://tableau.stonybrook.edu/vizql/t/Public/w/Covid-19Accessible/v/WeeklyCases/viewData/sessions/D14FD08477834B44B614D86439F691FC-1:3/views/14068672024067378307_6414820709505016832?maxrows=200&viz={"worksheet"%3A"SBU Cases Acc"%2C"dashboard"%3A"Weekly Cases"}')
 
 html = driver.page_source
+#print(html)
 soup = BeautifulSoup(html)
-
+print(soup)
+#html.context
 print(soup.find("table"))
+driver.close()
